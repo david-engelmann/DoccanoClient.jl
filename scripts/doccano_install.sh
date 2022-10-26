cd099de6761cde000cc721a69890c8173a31d478
david-engelmann <david@satisfilabs.com>
2022-10-26 16:50:31 -0400

    hide doccano setup output

--- scripts/doccano_install.sh
+++ scripts/doccano_install.sh
@@ -29,5 +29,5 @@ echo "POSTGRES_PASSWORD=doccano" >> .env
 echo "POSTGRES_DB=doccano" >> .env
 
 # Create Doccano with docker-compose
-docker-compose -f docker/docker-compose.prod.yml --env-file .env up
+docker-compose -f docker/docker-compose.prod.yml --env-file .env up &
 exec "$@"
