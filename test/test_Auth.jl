93e14d193cfdeb4747e3f7e42610df3d5a2e1b2a
david-engelmann <david@satisfilabs.com>
2022-10-26 14:21:13 -0400

    adjust login test to use docker.env vars

--- test/test_Auth.jl
+++ test/test_Auth.jl
@@ -7,9 +7,15 @@ using Test
 end
 
 @testset "Login" begin
-    username = "admin"
-    password = "password"
-    base_url = "http://127.0.0.1"
+    try
+        username = ENV["DOCCANO_USERNAME"]
+        password = ENV["DOCCANO_PASSWORD"]
+        base_url = ENV["DOCCANO_BASE_URL"]
+    catch err
+        username = "admin"
+        password = "password"
+        base_url = "http://127.0.0.1"
+    end
     version = "v1"
     @test login(base_url, username, password, csrf_token, version) == csrf_token
 end
