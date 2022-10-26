6af13241dada5d8fc0978d2862a80b0580423775
david-engelmann <david@satisfilabs.com>
2022-10-26 16:29:07 -0400

    install docker-compose and spin up doccano

--- scripts/doccano_install.sh
+++ scripts/doccano_install.sh
@@ -3,6 +3,9 @@ apt update
 apt upgrade -y
 apt install git curl -y
 
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 git clone https://github.com/doccano/doccano.git --config core.autocrlf=input
 cd doccano
 
@@ -22,5 +25,13 @@ echo "POSTGRES_USER=doccano" >> .env
 echo "POSTGRES_PASSWORD=doccano" >> .env
 echo "POSTGRES_DB=doccano" >> .env
 
-cat .env
+docker-compose -f docker/docker-compose.prod.yml --env-file .env up
+
+if ping -c 1 http://127.0.0.1/ &> /dev/null
+then
+  echo "Doccano Installed"
+else
+  echo "Doccano Installation Failed"
+fi
+
 exec "$@"
