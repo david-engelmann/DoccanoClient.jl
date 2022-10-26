5cc7cdb1f8eedd46fe5acd1d9d47d412effc2af1
david-engelmann <david@satisfilabs.com>
2022-10-26 18:17:50 -0400

    export doccano variables for workflows

--- scripts/doccano_login.sh
+++ scripts/doccano_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Test Doccano Login
-DOCCANO_BASE_URL=localhost
-DOCCANO_USERNAME="admin"
-DOCCANO_PASSWORD="password"
+export DOCCANO_BASE_URL=localhost
+export DOCCANO_USERNAME="admin"
+export DOCCANO_PASSWORD="password"
 julia test/test_Auth.jl
 exec "$@"
