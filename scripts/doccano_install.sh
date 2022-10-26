fb43d5eea0137a35ff8b32451699da02fa73ce49
david-engelmann <david@satisfilabs.com>
2022-10-26 16:58:15 -0400

    change to correct ping package name

--- scripts/doccano_install.sh
+++ scripts/doccano_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install git curl ping -y
+apt install git curl iputils-ping -y
 
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
