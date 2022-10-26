a18102d1df0a6a205288030bc8c2a3d77844d1fd
David Engelmann <81196676+david-engelmann@users.noreply.github.com>
2022-10-26 19:33:05 -0400

    add http:// back

--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -2,14 +2,14 @@
 # Let Doccano Finish Spinning Up
 sleep 60
 # Test the Doccano Instance with ping
-if ping -c 1 127.0.0.1 &> /dev/null
+if ping -c 1 http://127.0.0.1 &> /dev/null
 then
   echo "Doccano Installed"
 else
   echo "Doccano Installation Failed"
 fi
 
-if ping -c 1 127.0.0.1/admin/login/ &> /dev/null
+if ping -c 1 http://127.0.0.1/admin/login/ &> /dev/null
 then
   echo "Doccano Admin Accessable"
 else
