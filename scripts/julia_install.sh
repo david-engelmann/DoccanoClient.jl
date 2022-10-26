bf4183fb1948edd66a7d7672acf00d87401c3d3d
david-engelmann <david@satisfilabs.com>
2022-10-26 18:02:21 -0400

    compile julia from source code

--- scripts/julia_install.sh
+++ scripts/julia_install.sh
@@ -13,6 +13,12 @@ set -xe; \
 apt-get update && apt-get upgrade -y && apt-get install -y apt-utils gcc g++ openssh-server cmake build-essential gdb gdbserver rsync locales
 apt-get install -y bzip2 wget gnupg dirmngr apt-transport-https tzdata ca-certificates openssh-server tmux && apt-get clean
 
+mkdir /opt/julia-${JULIA_VERSION} && \
+    cd /tmp && \
+    wget -q https://julialang-s3.julialang.org/bin/linux/x64/`echo ${JULIA_VERSION} | cut -d. -f 1,2`/julia-${JULIA_VERSION}-linux-x86_64.tar.gz && \
+    tar xzf julia-${JULIA_VERSION}-linux-x86_64.tar.gz -C /opt/julia-${JULIA_VERSION} --strip-components=1 && \
+    rm /tmp/julia-${JULIA_VERSION}-linux-x86_64.tar.gz
+
 julia -e 'using Pkg; Pkg.add("HTTP", preserve=PRESERVE_DIRECT);'
 
 julia -e 'using Pkg; Pkg.add("URIs", preserve=PRESERVE_DIRECT);'
