fde370681fb7c1847b532404a733ad19837a07c9
David Engelmann <david+4113@satisfilabs.com>
2022-11-02 19:42:11 -0400

    set port and localhost

--- scripts/doccano_login.sh
+++ scripts/doccano_login.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Test Doccano Login
-export DOCCANO_BASE_URL="http://0.0.0.0:8000"
+export DOCCANO_BASE_URL="http://localhost:8000"
 export DOCCANO_USERNAME="admin"
 export DOCCANO_PASSWORD="password"
 julia test/test_Auth.jl
