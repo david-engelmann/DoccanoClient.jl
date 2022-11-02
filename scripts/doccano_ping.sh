a3ad3ddb9281cef21b0442aeaa5f0662104eaeb4
David Engelmann <david+4113@satisfilabs.com>
2022-11-02 16:18:13 -0400

    try to ping with port 80

--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -4,14 +4,14 @@ sleep 60
 
 cat /etc/hosts
 # Test the Doccano Instance with ping
-if ping -c 1 http://127.0.0.1 &> /dev/null
+if ping -c 1 http://127.0.0.1:80 &> /dev/null
 then
   echo "Doccano Installed"
 else
   echo "Doccano Installation Failed"
 fi
 
-if ping -c 1 http://127.0.0.1/admin/login/ &> /dev/null
+if ping -c 1 http://127.0.0.1:80/admin/login/ &> /dev/null
 then
   echo "Doccano Admin Accessable"
 else
