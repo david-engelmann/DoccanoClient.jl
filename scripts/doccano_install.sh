6f2df4e3546e19dc20c7bb563b9eb1c2f417e662
David Engelmann <david+4113@satisfilabs.com>
2022-11-02 17:08:03 -0400

    remove refs to sudo

--- scripts/doccano_install.sh
+++ scripts/doccano_install.sh
@@ -5,10 +5,10 @@ apt install ca-certificates gnupg lsb-release git curl iputils-ping -y
 
 # Install docker
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 chmod a+r /etc/apt/keyrings/docker.gpg
 apt-get update
