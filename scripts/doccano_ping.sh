bb59b11571d4610f3d142e14149f913d50b7e41f
David Engelmann <81196676+david-engelmann@users.noreply.github.com>
2022-11-02 20:56:18 -0400

    port to 8000 for ping (step)

--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -4,14 +4,14 @@ sleep 60
 
 cat /etc/hosts
 # Test the Doccano Instance with ping
-if ping -c 1 http://0.0.0.0:8888 &> /dev/null
+if ping -c 1 http://0.0.0.0:8000 &> /dev/null
 then
   echo "Doccano Installed"
 else
   echo "Doccano Installation Failed"
 fi
 
-if ping -c 1 http://0.0.0.0:8888/admin/login/ &> /dev/null
+if ping -c 1 http://0.0.0.0:8000/admin/login/ &> /dev/null
 then
   echo "Doccano Admin Accessable"
 else
