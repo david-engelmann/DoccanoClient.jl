33f62fff64a0fe48425b85b0fc80a595fd044429
David Engelmann <81196676+david-engelmann@users.noreply.github.com>
2022-11-02 21:11:21 -0400

    PING EVERYTHING

--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -4,12 +4,31 @@ sleep 60
 
 cat /etc/hosts
 # Test the Doccano Instance with ping
+echo "----------- test localhost -----------"
+ping -c 1 http://localhost &> /dev/null
+
+echo ""
+echo "----------- test localhost:8000 -------------"
+ping -c 1 http://localhost:8000
+
+echo ""
+echo "----------- test localhost:8888 --------------"
+ping -c 1 http://localhost:8888 &> /dev/null
+
+echo ""
+echo "----------- test 0.0.0.0:8888 -----------------"
+ping -c 1 http://0.0.0.0:8888 &> /dev/null
+
+echo ""
+echo "----------- test 0.0.0.0:8000 -----------------"
 if ping -c 1 http://0.0.0.0:8000 &> /dev/null
 then
   echo "Doccano Installed"
 else
   echo "Doccano Installation Failed"
 fi
+
+echo ""
 
 if ping -c 1 http://0.0.0.0:8000/admin/login/ &> /dev/null
 then
