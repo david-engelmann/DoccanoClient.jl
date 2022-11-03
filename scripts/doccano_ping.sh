5dbfc878c78c6d8d1f26e69a1d8c80095a9e0c4e
David Engelmann <81196676+david-engelmann@users.noreply.github.com>
2022-11-02 21:37:49 -0400

    clean up bash output with linebreaks

--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -6,30 +6,44 @@ cat /etc/hosts
 # Test the Doccano Instance with ping
 echo "----------- test localhost -----------"
 ping -c 1 http://localhost &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test localhost:8000 -------------"
 ping -c 1 http://localhost:8000 &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test localhost:8888 --------------"
 ping -c 1 http://localhost:8888 &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test 0.0.0.0:8888 -----------------"
 ping -c 1 http://0.0.0.0:8888 &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test 127.0.0.1 -----------------"
 ping -c 1 http://127.0.0.1 &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test 127.0.0.1:8000 -----------------"
 ping -c 1 http://127.0.0.1:8000 &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test 127.0.0.1:8888 -----------------"
 ping -c 1 http://127.0.0.1:8888 &> /dev/null
+echo
+sleep 1
 
 echo ""
 echo "----------- test 0.0.0.0:8000 -----------------"
@@ -41,6 +55,7 @@ else
 fi
 
 echo ""
+sleep 1
 
 if ping -c 1 http://0.0.0.0:8000/admin/login/ &> /dev/null
 then
