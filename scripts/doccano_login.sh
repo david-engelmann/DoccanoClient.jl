0b044ae4b6316f0b4ca7587f1e3a239984fb3ff7
David Engelmann <david+4113@satisfilabs.com>
2022-11-07 10:46:09 -0500

    use $PING_DOCCANO_ID in login

--- scripts/doccano_login.sh
+++ scripts/doccano_login.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Test Doccano Login
 echo "Test Login"
-export DOCCANO_BASE_URL="http://0.0.0.0"
+export DOCCANO_BASE_URL="http://$PING_DOCCANO_ID"
+echo $DOCCANO_BASE_URL
 export DOCCANO_USERNAME="admin"
 export DOCCANO_PASSWORD="password"
 julia test/test_Auth.jl
