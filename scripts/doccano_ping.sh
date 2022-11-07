bd868f086566833e2bbb8245a079fd7990d1b747
David Engelmann <david+4113@satisfilabs.com>
2022-11-07 14:12:41 -0500

    increase sleep 1 to sleep 10

--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -9,14 +9,14 @@ echo "----------- test 172.18.0.3 -------------------"
 ping -c 1 172.18.0.3 &> /dev/null
 echo ""
 echo ""
-sleep 1
+sleep 10
 
 echo ""
 echo "----------- test localhost --------------------"
 ping -c 1 localhost &> /dev/null
 echo ""
 echo ""
-sleep 1
+sleep 10
 
 echo ""
 echo "----------- test http://172.18.0.3 -----------------"
@@ -28,7 +28,7 @@ else
 fi
 echo ""
 echo ""
-sleep 1
+sleep 10
 
 echo ""
 echo "--------- test http://172.18.0.1/admin/login/ -----------"
@@ -40,7 +40,7 @@ else
 fi
 echo ""
 echo ""
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------- test http://0.0.0.0/admin/login/ -----------"
@@ -50,7 +50,7 @@ then
 else
   echo "Doccano Admin Access Failed"
 fi
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------- test http://localhost/admin/login/ -----------"
@@ -60,7 +60,7 @@ then
 else
   echo "Doccano Admin Access Failed"
 fi
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------- test http://172.18.0.3/admin/login/ -----------"
@@ -70,12 +70,12 @@ then
 else
   echo "Doccano Admin Access Failed"
 fi
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "------------ test doccano ---------------------"
 ping -c 1 doccano
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "------------ set PING_DOCCANO_ID with doccano from grep ------------------"
@@ -85,55 +85,55 @@ echo ""
 echo "------------ test ping PING_DOCCANO_ID ---------------------"
 echo $PING_DOCCANO_ID
 ping -c 1 $PING_DOCCANO_ID
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------------------- test PING_DOCCANO_ID:6379 -----------------"
 ping -c 1 $PING_DOCCANO_ID:6379
-sleep 1
+sleep 10
 echo ""
 echo ""
 
 echo "--------------------- test PING_DOCCANO_ID:6379/tcp -----------------"
 ping -c 1 $PING_DOCCANO_ID:6379/tcp
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "------------ test ping http://PING_DOCCANO_ID + awk ---------------------"
 echo $PING_DOCCANO_ID
 ping -c 1 http://$PING_DOCCANO_ID
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------------------- test http://PING_DOCCANO_ID:6379 -----------------"
 ping -c 1 http://$PING_DOCCANO_ID:6379
-sleep 1
+sleep 10
 echo ""
 echo ""
 
 echo "--------------------- test http://PING_DOCCANO_ID:6379/tcp -----------------"
 ping -c 1 http://$PING_DOCCANO_ID:6379/tcp
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "------------ test ping 0.0.0.0 ---------------------"
 ping -c 1 0.0.0.0
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------------------- test 0.0.0.0:6379 -----------------"
 ping -c 1 0.0.0.0:6379
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "--------------------- test http://0.0.0.0 ----------------"
 ping -c 1 http://0.0.0.0
-sleep 1
+sleep 10
 echo ""
 echo ""
 echo "-------------------- test http://0.0.0.0:6379 -----------------"
 ping -c 1 http://0.0.0.0:6379
-sleep 1
+sleep 10
 echo ""
 echo ""
 
