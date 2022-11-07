4253ac56f16e8277ce3407a38932bc7fddcb34fe
David Engelmann <david+4113@satisfilabs.com>
2022-11-07 08:07:08 -0500

    change login endpoint to 0.0.0.0

--- scripts/doccano_login.sh
+++ scripts/doccano_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Test Doccano Login
 echo "Test Login"
-export DOCCANO_BASE_URL="doccano"
+export DOCCANO_BASE_URL="0.0.0.0"
 export DOCCANO_USERNAME="admin"
 export DOCCANO_PASSWORD="password"
 julia test/test_Auth.jl
