5ee790ffa95e4649170a18d95f98d17046c575bb
David Engelmann <david+4113@satisfilabs.com>
2022-11-08 16:28:16 -0500

    add in echo for github action display

--- scripts/doccano_full_test.sh
+++ scripts/doccano_full_test.sh
@@ -2,6 +2,7 @@
 export DOCCANO_BASE_URL="http://${PING_DOCCANO_ID}:6379/"
 export DOCCANO_USERNAME="admin"
 export DOCCANO_PASSWORD="password"
+echo "------------------------ Starting Full Test --------------------------------"
 julia test/test_Full.jl
 exec "$@"
 exec "$@"
