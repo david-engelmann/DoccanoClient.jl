0d75c0578b7c65b3f0991be88a57bc306bfa5d02
David Engelmann <david+4113@satisfilabs.com>
2022-11-08 15:06:18 -0500

    clean up ping doc to be 1 call, try no doc install step

--- scripts/doccano_login.sh
+++ scripts/doccano_login.sh
@@ -3,12 +3,7 @@
 echo "Test Login"
 echo "----------- Doccano Ip Address ------------"
 echo ${PING_DOCCANO_ID}
-echo ""
-echo "----------- Doccano Docker ID"
-echo ${PING_DOCKER_ID}
 export DOCCANO_BASE_URL="http://${PING_DOCCANO_ID}:6379/"
-echo $DOCCANO_BASE_URL
-#export DOCCANO_BASE_URL="http://doccano:6379/"
 export DOCCANO_USERNAME="admin"
 export DOCCANO_PASSWORD="password"
 julia test/test_Auth.jl
--- scripts/doccano_ping.sh
+++ scripts/doccano_ping.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
-# Let Doccano Finish Spinning Up
-sleep 60
-
-cat /etc/hosts
-
-echo ""
-echo ""
-echo "--------- test http://172.18.0.3:6379/admin/login/ -----------"
-if ping -c 1 http://172.18.0.3:6379/admin/login/ &> /dev/null
-then
-  echo "Doccano Admin Accessable"
-else
-  echo "Doccano Admin Access Failed"
-fi
-sleep 10
-echo ""
-echo ""
-echo "--------- test http://doccano:6379/admin/login/ -----------"
-if ping -c 1 http://doccano:6379/admin/login/ &> /dev/null
-then
-  echo "Doccano Admin Accessable"
-else
-  echo "Doccano Admin Access Failed"
-fi
-sleep 10
-echo ""
-echo ""
 echo "------------ set PING_DOCCANO_ID with doccano from grep ------------------"
 export PING_DOCCANO_ID=$(ping -c 1 doccano | grep -Po '(?<=\().*(?=\))' | grep -Po '.*(?=\))')
-echo ""
 echo ""
 echo "------------ test ping PING_DOCCANO_ID ---------------------"
 echo "PING_DOCCANO_ID=$PING_DOCCANO_ID" >> $GITHUB_ENV
 echo ""
-echo ""
-echo "--------------------- test http://PING_DOCCANO_ID:6379/ -----------------"
-ping -c 1 http://$PING_DOCCANO_ID:6379/
+echo "--------- test http://$PING_DOCCANO_ID:6379/admin/login/ -----------"
+if ping -c 1 http://$PING_DOCCANO_ID:6379/admin/login/ &> /dev/null
+then
+  echo "Doccano Admin Accessable"
+else
+  echo "Doccano Admin Access Failed"
+fi
 sleep 10
 export PING_DOCKER_ID=$(docker ps | grep "doccano\s" | awk '{ print $1 }')
 echo "PING_DOCKER_ID=$PING_DOCKER_ID" >> $GITHUB_ENV 
