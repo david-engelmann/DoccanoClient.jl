7d8324c017a8741f7d6b12831bf14c41b81484d8
David Engelmann <david+4113@satisfilabs.com>
2022-11-15 17:18:26 -0500

    add defaults for comment struct

--- src/models/Comment.jl
+++ src/models/Comment.jl
@@ -1,8 +1,8 @@
-struct Comment
-    id::Integer
+Base.@kwdef struct Comment
+    id::Union{Integer, nothing} = nothing
     text::String
     example::Integer
-    user::Integer
-    username::String
-    created_at::String
+    user::Union{Integer, nothing} = nothing
+    username::Union{String, nothing} = nothing
+    created_at::Union{String, nothing} = nothing
 end
