d9f238034c10b8448288bffa436c9673a912b300
David Engelmann <david+4113@satisfilabs.com>
2022-11-15 16:23:16 -0500

    add valid suffix and prefix keys and text and color constraints

--- src/models/LabelType.jl
+++ src/models/LabelType.jl
@@ -1,3 +1,52 @@
+
+VALID_PREFIX_KEY = ["ctrl", "shift", "ctrl shift"]
+VALID_SUFFIX_KEY = ["0",
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5",
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    "a",
+                    "b",
+                    "c",
+                    "d",
+                    "e",
+                    "f",
+                    "g",
+                    "h",
+                    "i",
+                    "j",
+                    "k",
+                    "l",
+                    "m",
+                    "n",
+                    "o",
+                    "p",
+                    "q",
+                    "r",
+                    "s",
+                    "t",
+                    "u",
+                    "v",
+                    "w",
+                    "x",
+                    "y",
+                    "z"]
+
+Base.@kwdef struct TextConstraint
+    min_length::Integer = 1
+    max_length::Integer = 100
+    strip_whitespace::Boolean = true
+end
+
+Base.@kwdef struct ColorConstraint
+    regex_validation::String = "#[a-fA-F0-9]{6}"
+end
+
 struct LabelType
     id::Union{Integer, nothing}
     text::String
