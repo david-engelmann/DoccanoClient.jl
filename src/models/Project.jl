4322af2d91de113f60fcd5aab72e86bbd28f2ffb
David Engelmann <david+4113@satisfilabs.com>
2022-11-15 15:54:45 -0500

    add default arguments to project struct

--- src/models/Project.jl
+++ src/models/Project.jl
@@ -1,14 +1,14 @@
-struct Project
-    id::Integer
+Base.@kwdef struct Project
+    id::Union{Integer, nothing}
     name::String
     description::String
     guideline::String
     project_type::String
-    random_order::Boolean
-    collaborative_annotation::Boolean
-    single_class_classification::Boolean
-    allow_overlapping::Boolean
-    grapheme_mode::Boolean
-    use_relation::Boolean
-    tags::Array
+    random_order::Boolean = false
+    collaborative_annotation::Boolean = false
+    single_class_classification::Boolean = false
+    allow_overlapping::Boolean = false
+    grapheme_mode::Boolean = false
+    use_relation::Boolean = false
+    tags::Array{String} = []
 end
