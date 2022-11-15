044c30e9769dd356a8227dac34ab5ea7509b3e92
David Engelmann <david+4113@satisfilabs.com>
2022-11-15 14:14:54 -0500

    add in example struct

---
+++ src/models/Example.jl
@@ -0,0 +1,11 @@
+struct Example
+    id::Integer
+    text::Integer
+    meta::Dict
+    annotation_approver::String
+    comment_count::Integer
+    is_confirmed::Boolean
+    filename::String
+    upload_name::String
+    score::Float
+end
