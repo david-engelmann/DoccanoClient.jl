bbe1c3f2292b0ff69dade3ba9143bb4a10068a41
David Engelmann <david+4113@satisfilabs.com>
2022-11-29 19:20:47 -0500

    fix base_url in Roles test

--- test/test_Roles.jl
+++ test/test_Roles.jl
@@ -2,6 +2,7 @@ include("../src/Roles.jl")
 using Test
 
 @testset "Roles - Get Functions" begin
+    #=
     try
         base_url = ENV["DOCCANO_BASE_URL"]
         test_project_id = ENV["DOCCANO_TEST_PROJECT_ID"]
@@ -9,6 +10,9 @@ using Test
         base_url = "http://127.0.0.1"
         test_project_id = 106
     end
+    =#
+    base_url = ENV["DOCCANO_BASE_URL"]
+    test_project_id = ENV["DOCCANO_TEST_PROJECT_ID"]
     roles = get_roles(base_url, csrf_token, "v1")
     target_role_id = 1
     rolemappings = get_rolemappings(base_url, test_project_id, csrf_token, "v1")
@@ -18,6 +22,7 @@ end
 
 
 @testset "Roles - Link Creation" begin
+    #=
     try
         base_url = ENV["DOCCANO_BASE_URL"]
         test_project_id = ENV["DOCCANO_TEST_PROJECT_ID"]
@@ -25,10 +30,15 @@ end
         base_url = "http://127.0.0.1"
         test_project_id = 106
     end
+    =#
+    base_url = ENV["DOCCANO_BASE_URL"]
+    test_project_id = ENV["DOCCANO_TEST_PROJECT_ID"]
     version = "v1"
     no_suffix_role_url = create_roles_url(base_url, version)
     no_suffix_project_role_url = create_project_roles_url(base_url, project_id, version)
 
-    @test no_suffix_role_url == "$(base_url)/$(version)/roles"
-    @test no_suffix_project_role_url = "$(base_url)/$(version)/projects/$(test_project_id)"
+    test_no_suffix_role_url = if endswith(base_url, raw"/") "$(base_url)$(version)/roles" else "$(base_url)/$(version)/roles" end
+    test_no_suffix_project_role_url = if endswith(base_url, raw"/") "$(base_url)$(version)/projects/$(test_project_id)" else "$(base_url)/$(version)/projects/$(test_project_id)" end
+    @test no_suffix_role_url == test_no_suffix_role_url
+    @test no_suffix_project_role_url = test_no_suffix_project_role_url
 end
