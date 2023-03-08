d2ee82aab7be8355a12d99a3c02297e0cd575b14
David Engelmann <david+4113@satisfilabs.com>
2023-03-08 16:41:43 -0500

    remove )) typo

--- src/Examples.jl
+++ src/Examples.jl
@@ -114,7 +114,7 @@ function create_example(base_url :: String, project_id :: Integer, _csrf_token :
     example_payload = Dict(["text" => text,
                          "meta" => meta])
     @info typeof(JSON3.write(example_payload))
-    @info JSON3.write(example_payload))
+    @info JSON3.write(example_payload)
     r = make_create_example_request(url, headers, JSON3.write(example_payload))
     return JSON3.read(r.body)
 end
@@ -128,7 +128,7 @@ function update_example(base_url :: String, project_id :: Integer, example_id ::
                          "meta" => meta])
     @info "example_payload"
     @info typeof(JSON3.write(example_payload))
-    @info JSON3.write(example_payload))
+    @info JSON3.write(example_payload)
     r = make_update_example_request(url, headers, JSON3.write(example_payload))
     return JSON3.read(r.body)
 end
