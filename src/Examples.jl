a9d369f62179fa793c13737e9429f7aa73a04076
David Engelmann <david+4113@satisfilabs.com>
2023-04-14 12:49:27 -0400

    duck url_parameters

--- src/Examples.jl
+++ src/Examples.jl
@@ -107,7 +107,11 @@ function get_all_examples(base_url :: String, project_id :: Integer, _csrf_token
 end
 
 function get_example_ids(base_url:: String, project_id :: Integer, _csrf_token :: String, url_parameters :: Union{Dict, Nothing}=nothing, version :: String="v1")
-    examples = get_all_examples(base_url, project_id, _csrf_token, url_parameters, version)
+    if url_parameters
+        examples = get_examples(base_url, project_id, _csrf_token, url_parameters, version)
+    else
+        examples = get_examples(base_url, project_id, _csrf_token, version)
+    end
     example_ids = [example for example in examples]
     return example_ids
 end
