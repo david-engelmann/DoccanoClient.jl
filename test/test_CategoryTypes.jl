51a333e4558091886aa7b39092bb63ef3852ea50
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 17:46:32 -0400

    typo in alpha

--- test/test_CategoryTypes.jl
+++ test/test_CategoryTypes.jl
@@ -5,7 +5,7 @@ using Test
     base_url = ENV["DOCCANO_BASE_URL"]
     test_project_id = 1
     version = "v1"
-    sample_category_types_file_name = "label_multi_label_alhpa.json"
+    sample_category_types_file_name = "label_multi_label_alpha.json"
     sample_category_types_file_path = "test/assets"
     upload_category_types(base_url, test_project_id, csrf_token, sample_category_types_file_name, sample_category_types_file_path)
     @test isa(base_url, String)
