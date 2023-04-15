5db836c472dc9cb1b6d7ec94fe29fbb5e4b501d0
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 17:14:19 -0400

    add test for get_category_types

--- test/test_CategoryTypes.jl
+++ test/test_CategoryTypes.jl
@@ -12,8 +12,14 @@ using Test
 end
 
 @testset "CategoryTypes - Get Functions" begin
-    @info "Testing getting"
-    @test false == true
+    base_url = ENV["DOCCANO_BASE_URL"]
+    test_project_id = 1
+    version = "v1"
+    category_types = get_category_types(base_url, test_project_id, csrf_token, version)
+    test_category_type_id = first(category_types["results"])["id"]
+    test_category_type_detail = get_category_type_detail(base_url, test_project_id, test_category_type_id, csrf_token)
+    @test length(category_types["results"]) == 5
+    @test isa(test_category_type_detail["text"], String)
 end
 
 @testset "CategoryTypes - CategoryType Id Functions" begin
