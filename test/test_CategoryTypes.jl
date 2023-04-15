5ed6e22dca2674b354d8d46cb7bd9c35d36f3305
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 17:06:33 -0400

    use @test not assert

--- test/test_CategoryTypes.jl
+++ test/test_CategoryTypes.jl
@@ -3,26 +3,26 @@ using Test
 
 @testset "CategoryTypes - Upload Functions" begin
     @info "Testing uploading"
-    assert false == true
+    @test false == true
 end
 
 @testset "CategoryTypes - Get Functions" begin
     @info "Testing getting"
-    assert false == true
+    @test false == true
 end
 
 @testset "CategoryTypes - CategoryType Id Functions" begin
     @info "Testing category_type_ids"
-    assert false == true
+    @test false == true
 end
 
 @testset "CategoryTypes - Update Functions" begin
     @info "Testing updating"
-    assert false == true
+    @test false == true
 end
 
 @testset "CategoryTypes - Link Creation" begin
     @info "Testing Link Creation"
-    assert false == true
+    @test false == true
 end
 
