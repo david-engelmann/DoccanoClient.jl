7a7a518be7d5ff35f9d853a2360626be742c3f7f
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 17:39:58 -0400

    change assest reference

--- test/test_CategoryTypes.jl
+++ test/test_CategoryTypes.jl
@@ -5,7 +5,7 @@ using Test
     base_url = ENV["DOCCANO_BASE_URL"]
     test_project_id = 1
     version = "v1"
-    sample_category_types_file_name = "sample_category_types.json"
+    sample_category_types_file_name = "label_multi_label_alhpa.json"
     sample_category_types_file_path = "test/assets"
     upload_category_types(base_url, test_project_id, csrf_token, sample_category_types_file_name, sample_category_types_file_path)
     @test isa(base_url, String)
@@ -26,7 +26,7 @@ end
     base_url = ENV["DOCCANO_BASE_URL"]
     test_project_id = 1
     version = "v1"
-    category_type_ids = get_category_type_ids(base_url, test_project_id, csrf_token, nothing, version)
+    category_type_ids = get_category_type_ids(base_url, test_project_id, csrf_token, version)
     @test length(category_type_ids) == 2
     @test category_type_ids == collect(1:2)
 end
