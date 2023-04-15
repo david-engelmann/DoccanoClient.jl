1d40c5ace427ff1e6b1190098a1704bcefd0895d
David Engelmann <david+4113@satisfilabs.com>
2023-04-14 20:10:51 -0400

    test for category types

---
+++ test/test_CategoryTypes.jl
@@ -0,0 +1,28 @@
+include("../src/CategoryTypes.jl")
+using Test
+
+@testset "CategoryTypes - Upload Functions" begin
+    @info "Testing uploading"
+    assert false
+end
+
+@testset "CategoryTypes - Get Functions" begin
+    @info "Testing getting"
+    assert false
+end
+
+@testset "CategoryTypes - CategoryType Id Functions" begin
+    @info "Testing category_type_ids"
+    assert false
+end
+
+@testset "CategoryTypes - Update Functions" begin
+    @info "Testing updating"
+    assert false
+end
+
+@testset "CategoryTypes - Link Creation" begin
+    @info "Testing Link Creation" begin
+    assert false
+end
+
