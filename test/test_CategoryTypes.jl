0854d5347222bff6ee366ca93957a24654f6f78b
David Engelmann <david+4113@satisfilabs.com>
2023-04-14 22:22:19 -0400

    complete placeholder assert

--- test/test_CategoryTypes.jl
+++ test/test_CategoryTypes.jl
@@ -3,26 +3,26 @@ using Test
 
 @testset "CategoryTypes - Upload Functions" begin
     @info "Testing uploading"
-    assert false
+    assert false == true
 end
 
 @testset "CategoryTypes - Get Functions" begin
     @info "Testing getting"
-    assert false
+    assert false == true
 end
 
 @testset "CategoryTypes - CategoryType Id Functions" begin
     @info "Testing category_type_ids"
-    assert false
+    assert false == true
 end
 
 @testset "CategoryTypes - Update Functions" begin
     @info "Testing updating"
-    assert false
+    assert false == true
 end
 
 @testset "CategoryTypes - Link Creation" begin
     @info "Testing Link Creation"
-    assert false
+    assert false == true
 end
 
