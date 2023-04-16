4971d75a8c6eab3fe755951e2f585a0650eca187
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 01:02:03 -0400

    inspect file_name

--- src/CategoryTypes.jl
+++ src/CategoryTypes.jl
@@ -156,6 +156,8 @@ function upload_category_types(base_url :: String, project_id :: Integer, _csrf_
     for file in files
         file_io = open(file, "r")
         file_name = create_uploadable_file_name(file)
+        @info "file_name:"
+        @info file_name
         upload_dict = Dict(["file" => HTTP.Multipart(file_name, file_io, "application/json")])
         #upload_dict = Dict(["file" => read(file_io, String)])
         @info "headers:"
