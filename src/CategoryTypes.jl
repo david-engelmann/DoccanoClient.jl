db44d77f0175050472da3bd3dd321987f3ff41bb
David Engelmann <david+4113@satisfilabs.com>
2023-04-15 20:39:31 -0400

    try with Multipart

--- src/CategoryTypes.jl
+++ src/CategoryTypes.jl
@@ -40,7 +40,7 @@ function make_update_category_type_request(url :: String, headers:: Vector{Pair{
     return HTTP.patch(url, headers, body=user_body; cookies = true)
 end
 
-function make_category_type_upload_request(url :: String, headers :: Vector{Pair{String, String}}, file_io :: Union{Dict{String, Union{IOStream, IOBuffer}}, HTTP.Form, Dict{String, String}})
+function make_category_type_upload_request(url :: String, headers :: Vector{Pair{String, String}}, file_io :: Union{Dict{String, Union{IOStream, IOBuffer}}, HTTP.Form, Dict{String, String}, Dict{String, HTTP.Multipart})
     return HTTP.post(url, headers, file_io; cookies = true)
 end
 
@@ -150,8 +150,8 @@ function upload_category_types(base_url :: String, project_id :: Integer, _csrf_
         file_io = open(file, "r")
         @info "file_io:"
         @info file_io
-        upload_dict = Dict(["file" => read(file_io, String)])
-
+        file_name = create_uploadable_file_name(file)
+        upload_dict = Dict(["file" => HTTP.Multipart(file_name, file_io])
         @info "upload_dict:"
         @info upload_dict
         r = make_category_type_upload_request(url, headers, upload_dict)
