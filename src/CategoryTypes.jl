a11eef044ebdf90a7221b7c24890fb3792cb9357
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 01:33:17 -0400

    file exist, need to pass isempty

--- src/CategoryTypes.jl
+++ src/CategoryTypes.jl
@@ -158,9 +158,9 @@ function upload_category_types(base_url :: String, project_id :: Integer, _csrf_
         file_name = create_uploadable_file_name(file)
         @info "file_name:"
         @info file_name
-        @info "file exist check:"
-        @info isfile(file)
-        upload_dict = Dict(["file" => HTTP.Multipart(file_name, JSON3.write(file_io), "application/json")])
+        @info "Check Multipart:"
+        @info HTTP.Multipart(file_name, file_io, "application/json")
+        upload_dict = Dict(["file" => HTTP.Multipart(file_name, file_io, "application/json")])
         #upload_dict = Dict(["file" => read(file_io, String)])
         @info "headers:"
         @info headers
