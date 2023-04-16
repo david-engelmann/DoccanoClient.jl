70f03268505fa671b33feff92a7f31d7f77ca638
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 02:39:04 -0400

    make content length a pair to add to headers

--- src/CategoryTypes.jl
+++ src/CategoryTypes.jl
@@ -160,7 +160,7 @@ function upload_category_types(base_url :: String, project_id :: Integer, _csrf_
         #upload_dict = Dict(["file" => multipart])
         upload_text = read(file_io, String)
         upload_dict = Dict(["file" => upload_text])
-        append!(headers, ["Content-Length: " * string(length(upload_text))])
+        append!(headers, ["Content-Length: " => string(length(upload_text))])
         @info "headers:"
         @info headers
         @info "upload_dict:"
