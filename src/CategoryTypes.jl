9bed6b94f4833bdf50af078ef9caef8aa83d13df
David Engelmann <david+4113@satisfilabs.com>
2023-04-16 04:31:32 -0400

    remove content length fully for now

--- src/CategoryTypes.jl
+++ src/CategoryTypes.jl
@@ -40,7 +40,7 @@ function make_update_category_type_request(url :: String, headers:: Vector{Pair{
     return HTTP.patch(url, headers, body=user_body; cookies = true)
 end
 
-function make_category_type_upload_request(url :: String, headers :: Vector{Pair{String, Any}}, file_io :: Union{
+function make_category_type_upload_request(url :: String, headers :: Vector{Pair{String, String}}, file_io :: Union{
                                                String,
                                                IOStream,
                                                IOBuffer,
