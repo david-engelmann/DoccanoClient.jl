8866ee7db0afe5e3133a1c70a6295364baa2b7fc
David Engelmann <david+4113@satisfilabs.com>
2023-04-17 09:19:17 -0400

    add content lenght and boundary

--- src/CategoryTypes.jl
+++ src/CategoryTypes.jl
@@ -158,7 +158,7 @@ function upload_category_types(base_url :: String, project_id :: Integer, _csrf_
         #upload_dict = Dict(["file" => multipart])
         #upload_text = read(file_io, String)
         upload_dict = HTTP.Form(Dict(["file" => multipart]))
-        headers = ["X-CSRFToken"=>_csrf_token, "Content-Type" =>  "multipart/form-data"]# , "Content-Length" => length(upload_text)]
+        headers = ["X-CSRFToken"=>_csrf_token, "Content-Type" =>  "multipart/form-data; boundary=----WebkitFormBoundaryxuRflWRLKyUvZXIa", "Content-Length" => length(file_io)]
         @info "headers:"
         @info headers
         @info "upload_dict:"
